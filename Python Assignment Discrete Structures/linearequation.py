51dfda057e7b98b2129be57c88658510733553e9
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2021-12-02 17:11:52 +0530

    done ✔️

---
+++ Python Assignment Discrete Structures/linearequation.py
@@ -0,0 +1,8 @@
+# List the integer solutions of a constrained linear equation.
+# x1 + x2 + x3 = 10
+cnt = 0
+for x1 in range(11):
+        for x2 in range(11):
+                for x3 in range(11):
+                        cnt += (x1 + x2 + x3 == 10)
+print(cnt)
