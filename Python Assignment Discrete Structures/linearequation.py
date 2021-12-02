ef0aa05997a719ca7095d356e92ccd3579f2230c
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2021-12-02 22:57:46 +0530

    no change ✔️

--- Python Assignment Discrete Structures/linearequation.py
+++ Python Assignment Discrete Structures/linearequation.py
@@ -6,5 +6,5 @@ for x1 in range(11):
                 for x3 in range(11):
                         cnt += (x1 + x2 + x3 == 10)
                         if(x1+x2+x3==10):
-                            print(x1, x2, x3)
+                                print(x1, x2, x3)
 print(cnt)
